b16cf14751a1fc149915ee1eaf1e2d21513538e6
Samuel Colin <scolin@hivernal.org>
2019-09-02 10:36:18 +0200

    Setting default font to DejaVu Sans Mono

--- custom.el
+++ custom.el
@@ -9,4 +9,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "outline" :slant normal :weight normal :height 98 :width normal))))
  '(whitespace-tab ((t (:foreground "#c1c1c1")))))
