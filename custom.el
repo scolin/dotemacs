5f7436097566b97b523ea3b78c628b67a480af36
Samuel Colin <scolin@hivernal.org>
2019-09-02 10:19:56 +0200

    Tracking custom.el

--- custom.el
+++ custom.el
@@ -3,10 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (exec-path-from-shell fullframe seq dash s))))
+ '(package-selected-packages (quote (org exec-path-from-shell fullframe seq dash s))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(whitespace-tab ((t (:foreground "#c1c1c1")))))
