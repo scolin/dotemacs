cd289b6f311d8b89fefc7c39816329a9e43011b4
Samuel Colin <scolin@hivernal.org>
2020-12-07 14:56:07 +0100

    local modifications

--- custom.el
+++ custom.el
@@ -3,11 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org exec-path-from-shell fullframe seq dash s))))
+ '(case-fold-search nil)
+ '(inhibit-startup-screen t)
+ '(org-export-with-sub-superscripts (quote {}))
+ '(package-selected-packages (quote (org exec-path-from-shell fullframe seq tuareg))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "outline" :slant normal :weight normal :height 98 :width normal))))
  '(whitespace-tab ((t (:foreground "#c1c1c1")))))
